5433581b5fa74539e39befe40f4aa1b29a073413
Mara Green <maragreen42@gmail.com>
2022-10-05 20:48:47 -0700

    add greeting funcition to start the story

---
+++ Hercules.py
@@ -0,0 +1,15 @@
+
+
+def print_greeting():
+    print("""
+    Hercules, it's about time you showed up! King Eurystheus has been waiting, it's never good to keep a king waiting. 
+    
+    You have been given three tasks: 
+        Slay the vicious Nemean Lion
+        Defeat the impossible nine-headed Lernaean Hydra
+        Capture the guard dog of the underword - Cerberus
+    Are you ready to get started? oh who am I kidding?! you don't get a choice...
+    """)
+
+
+print_greeting()
