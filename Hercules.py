aabc43fa3378b60cb30368d441ed8937ef1310c6
Mara Green <maragreen42@gmail.com>
2022-10-06 18:56:40 -0700

    add dictonaries for Hercules and the enemies. Add function names

--- Hercules.py
+++ Hercules.py
@@ -12,4 +12,58 @@ def print_greeting():
     """)
 
 
-print_greeting()
+#each char starts with 100 health points
+#list of attatcks and points
+
+hercules = {
+    'health': 100,
+    'attack power': 25,
+    'attack 1': 'Sword Slash',
+    'attack 2': 'Flying Side Kick',
+    'attatck 3': 'Skull Crusher'
+}
+
+lion = {
+    'health': 100,
+    'attack power': 20,
+    'attack 1': 'Raging Claw Strike',
+    'attack 2': 'Deafening Roar',
+    'attack 3': 'Razor Bite'
+}
+
+hydra = {
+    'health': 100,
+    'attack power': 20,
+    'attack 1': 'Vemon Bite',
+    'attack 2': 'Fire Ball',
+    'attack 3': 'Multi Razor Bites'
+}
+
+cerberus = {
+    'health': 100,
+    'attack power': 20,
+    'attack 1': 'Snake Tail Whip',
+    'attack 2': 'Acid Drool',
+    'attack 3': 'Shredding Bite'
+}
+
+def hercules_attatck():#should be able to choose attack
+    pass
+
+
+def lion_attack(): #random attatck 
+    pass
+
+def hydra_attack(): #random attatck
+    pass
+
+def cerberus_attatck(): #random attatck
+    pass
+
+def attack(): #As a developer, I want to use an Attack() function that will terminate when Hercules or an enemy’s health reaches zero.
+    pass
+
+def run_game():
+    print_greeting()
+
+run_game()
