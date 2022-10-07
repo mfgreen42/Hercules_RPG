896abbc451f65b3649134f4e4241a598bef6613d
Mara Green <maragreen42@gmail.com>
2022-10-06 19:47:47 -0700

    finish hercules_attack_lion function. Tested and runs

--- Hercules.py
+++ Hercules.py
@@ -25,7 +25,7 @@ hercules = {
 
 lion = {
     'health': 100,
-    'attack power': 20,
+    'attack power': 15,
     'attack 1': 'Raging Claw Strike',
     'attack 2': 'Deafening Roar',
     'attack 3': 'Razor Bite'
@@ -33,7 +33,7 @@ lion = {
 
 hydra = {
     'health': 100,
-    'attack power': 20,
+    'attack power': 15,
     'attack 1': 'Vemon Bite',
     'attack 2': 'Fire Ball',
     'attack 3': 'Multi Razor Bites'
@@ -41,23 +41,49 @@ hydra = {
 
 cerberus = {
     'health': 100,
-    'attack power': 20,
+    'attack power': 15,
     'attack 1': 'Snake Tail Whip',
     'attack 2': 'Acid Drool',
     'attack 3': 'Shredding Bite'
 }
 
-def hercules_attatck():#should be able to choose attack
+def hercules_attack_lion():#should be able to choose attack
+        user_input = input('what attack would you like to use against the vicious Nemean Lion ? Sword Slash, Flying Side Kick or Skull Crusher :')
+        if user_input == 'Sword Slash':
+            lion['health'] = lion['health'] - 25
+            if lion['health'] == 0:
+                print('You have defeated the vicious Nemean Lion!! Do you think you can handle the impossible nine-headed Lernaean Hydra? ')
+            else:
+                print(f'Sword Slash caused 25 damage points. The Nemean Lions health is now {lion["health"]}')
+        if user_input == 'Flying Side Kick':
+            lion['health'] = lion['health'] - 25
+            if lion['health'] == 0:
+                print('You have defeated the vicious Nemean Lion!! Do you think you can handle the impossible nine-headed Lernaean Hydra? ')
+            else:
+                print(f'Flying Side Kick caused 25 damage points. The Nemean Lions health is now {lion["health"]}')
+        if user_input == 'Skull Crusher':
+            lion['health'] = lion['health'] - 25
+            if lion['health'] == 0:
+                print('You have defeated the vicious Nemean Lion!! Do you think you can handle the impossible nine-headed Lernaean Hydra? ')
+            else:
+                print(f'Skull Crusher caused 25 damage points. The Nemean Lions health is now {lion["health"]}')
+
+def hercules_attack_hydra():
+    pass
+
+def hercules_attack_cerberus():
     pass
 
 
 def lion_attack(): #random attatck 
     pass
 
+
+
 def hydra_attack(): #random attatck
     pass
 
-def cerberus_attatck(): #random attatck
+def cerberus_attack(): #random attatck
     pass
 
 def attack(): #As a developer, I want to use an Attack() function that will terminate when Hercules or an enemy’s health reaches zero.
@@ -65,5 +91,10 @@ def attack(): #As a developer, I want to use an Attack() function that will term
 
 def run_game():
     print_greeting()
+    hercules_attack_lion()
+    lion_attack()
+
+
+
 
 run_game()
